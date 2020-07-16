3b76d2a7da5fec3fd3f3efbc9840da381afa2448
Reed Schalo <reed0schalo@gmail.com>
2020-07-15 18:34:19 -0700

    feat: added testing for helper functions

--- test/unit/controllers.users.test.js
+++ test/unit/controllers.users.test.js
@@ -1,5 +1,7 @@
 process.env.NODE_ENV = 'test';
 
+const fs = require('fs');
+const path = require('path');
 const chai = require('chai');
 const should = chai.should();
 
@@ -31,6 +33,19 @@ describe('controllers : users', () => {
       usersController.filterByYear(userArray, 2015, (err, total) => {
         should.not.exist(err);
         total.length.should.eql(2);
+        done();
+      });
+    });
+  });
+});
+
+describe('filterByYear() with helper', () => {
+  it('should return all users created on or after (>=) specified year', (done) => {
+    const testDataFile = path.join(__dirname, '..', 'test.data.json');
+    fs.readFile(testDataFile, 'utf8', (err, data) => {
+      usersController.filterByYear(JSON.parse(data), 2015, (err, total) => {
+        should.not.exist(err);
+        total.length.should.eql(5);
         done();
       });
     });
